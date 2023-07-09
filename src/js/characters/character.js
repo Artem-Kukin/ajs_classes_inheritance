8f5b5ebd0a42452d1dda4d8bfb2691a9a5d5ece8
Artem <a.ku666@vk.com>
2023-07-09 13:31:15 +0200

    repair

--- src/js/characters/character.js
+++ src/js/characters/character.js
@@ -1,6 +1,6 @@
 export default class Character {
   constructor(name, type) {
-    if (typeof name === 'string' && name.length > 2 && name.length < 11) {
+    if ((typeof name === 'string') && (name.length > 2 && name.length < 11)) {
       this.name = name;
     } else {
       throw new Error('Не корректное имя пользователя. Имя должно быть не менее 2-х и не более 10-ти символов');
@@ -16,11 +16,30 @@ export default class Character {
     if (types.includes(type)) {
       this.type = type;
     } else {
-      throw new Error(`Персонаж ${type} не существует`);
+      throw new Error(`Персонаж ${type} не существет`);
     }
     this.health = 100;
     this.level = 1;
     this.attack = undefined;
     this.defence = undefined;
+  }
+
+  levelUp() {
+    if (this.health !== 0) {
+      this.level += 1;
+      this.attack += this.attack * 0.2;
+      this.defence += this.defence * 0.2;
+      this.health = 100;
+    } else {
+      throw new Error('Нельзя повысить левел умершего');
+    }
+  }
+
+  damage(points) {
+    if (this.health > 0) {
+      this.health -= (points * (1 - this.defence / 100));
+    } else {
+      throw new Error('Нельзя атаковать умершего');
+    }
   }
 }
